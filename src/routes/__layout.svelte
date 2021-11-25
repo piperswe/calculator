7b1fc8cbea3814064c69ebeccbc75d73d094eb45
Piper McCorkle <contact@piperswe.me>
2021-11-24 20:21:19 -0600

    Use a side nav

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,35 @@
 <script lang="ts">
-	import { Content, Header, HeaderNav, HeaderNavItem } from 'carbon-components-svelte';
+	import {
+		Content,
+		Header,
+		SideNav,
+		SideNavItems,
+		SideNavLink,
+		SkipToContent
+	} from 'carbon-components-svelte';
+	import { Car20 } from 'carbon-icons-svelte';
 	import { page } from '$app/stores';
 	import '../app.scss';
+
+	let isSideNavOpen = false;
 </script>
 
-<Header company="Piper's" platformName="Calculators" href="/">
-	<HeaderNav>
-		<HeaderNavItem
+<Header company="Piper's" platformName="Calculators" href="/" bind:isSideNavOpen>
+	<div slot="skip-to-content">
+		<SkipToContent />
+	</div>
+</Header>
+
+<SideNav bind:isOpen={isSideNavOpen} rail>
+	<SideNavItems>
+		<SideNavLink
+			icon={Car20}
 			href="/autoloan"
 			text="Auto Loan"
 			isSelected={$page.path.startsWith('/autoloan')}
 		/>
-	</HeaderNav>
-</Header>
+	</SideNavItems>
+</SideNav>
 
 <Content>
 	<slot />
